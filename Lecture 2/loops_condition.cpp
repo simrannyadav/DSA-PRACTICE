a2ed55502ed516ade38bae977393aa8183c89a03
simrannyadav <yadav.simran9902@gmail.com>
2023-07-27 15:02:26 +0530

    updated

---
+++ Lecture 2/loops_condition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>> n;
+    cout<< "Value of n is: "<< n <<endl;
+    
+    if (n>0){
+        cout<< n<< "is positive"<<endl;
+    }
+    else{
+        cout<< n<< "is negative"<<endl;
+    }
+
+}
---
+++ Lecture 2/signed_unsigned.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int a = -112;
+    cout<<a<<endl;
+
+    int b = 2 / 5;
+    cout<< b <<endl;
+
+    cout<< 2.0/5 <<endl;
+
+    
+    return 0;
+}
