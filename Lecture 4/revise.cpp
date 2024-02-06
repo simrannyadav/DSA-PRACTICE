151463037f77e3de259c1786812cc6184459c4c2
simrannyadav <yadav.simran9902@gmail.com>
2024-02-06 13:50:06 +0530

    update

--- Lecture 4/revise.cpp
+++ Lecture 4/revise.cpp
@@ -3,22 +3,26 @@ using namespace std;
 
 int main(){
     int n;
-    cout<<"Enter n: ";
+    cout<<"Enter no of rows:";
     cin>>n;
-    int i = 1;  
-    while(i<=n){
-        int j = 1; 
-        char ch = 'A' + n - i;     
-        while(j<=i){
-            cout<<ch;
-            cout<<"  ";
-            j++;  
-            ch++;       
+    int i = n;
+    while(i>=1){
+        int space = n - i;
+        //space
+        while(space){
+            cout<<" ";
+            space--;
         }
-        i++;
-        
-        cout<<endl;
+        int j = 1;
+        while(j<=n){
+            cout<<"*";
+            j++;
+        }
+    cout<<endl;
+    i--;
     }
 }
+
+
 
 
